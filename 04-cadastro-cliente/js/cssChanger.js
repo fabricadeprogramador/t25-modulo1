b108627bf22397761a50cf7f3b7aa373d7e37d95
Vinicius Souza <viniciuspodi@gmail.com>
2019-03-07 20:40:46 -0400

    Finalizando versão 1 do cadastro de cliente

---
+++ 04-cadastro-cliente/js/cssChanger.js
@@ -0,0 +1,11 @@
+function abrirMenu(){
+    //Adiciona uma classe css chamada de nome-da-classe-css no elemento nome-elemento
+    document.getElementById("nome-elemento").classList.add("nome-da-classe-css")
+
+    //Remove uma classe css chamada de nome-da-classe-css no elemento nome-elemento
+    document.getElementById("nome-elemento").classList.remove("nome-da-classe-css")
+}
+
+function fecharMenu(){
+
+}
