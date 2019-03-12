42b38489e75ae2d2321cb1531fd81d80da2b4ee8
Vinicius Souza <viniciuspodi@gmail.com>
2019-03-12 19:56:26 -0400

    Corringindo JS abrir menu

--- 04-cadastro-cliente/js/cssChanger.js
+++ 04-cadastro-cliente/js/cssChanger.js
@@ -1,11 +1,13 @@
 function abrirMenu(){
     //Adiciona uma classe css chamada de nome-da-classe-css no elemento nome-elemento
-    document.getElementById("nome-elemento").classList.add("nome-da-classe-css")
+    document.getElementById("menu").classList.add("menu-aberto")
+    document.getElementById("menu").classList.remove("menu-fechado")
 
     //Remove uma classe css chamada de nome-da-classe-css no elemento nome-elemento
-    document.getElementById("nome-elemento").classList.remove("nome-da-classe-css")
+    // document.getElementById("nome-elemento").classList.remove("nome-da-classe-css")
 }
 
 function fecharMenu(){
-
+    document.getElementById("menu").classList.add("menu-fechado")
+    document.getElementById("menu").classList.remove("menu-aberto")
 }
