b604923c04b5eb25425086408d66f4a063a98d79
Vinicius Souza <viniciuspodi@gmail.com>
2019-03-14 21:16:52 -0400

    Finalizando validador

--- 04-cadastro-cliente/js/Validador.js
+++ 04-cadastro-cliente/js/Validador.js
@@ -1,11 +1,60 @@
-class Validador{
+class Validador {
 
-    validar(){
+    validar() {
 
+        let mensagem = ""
         let nome = document.getElementById("inputNome").value
+        let email = document.getElementById("email").value
+        let sexo = document.querySelector("[type=radio]:checked")
+        let qtdCursos = document.querySelectorAll("[type=checkbox]:checked").length
+        let estado = document.getElementById("estado").value
+        let foto = document.getElementById("foto").files[0]
+        let dataNascimento = document.getElementById("datanasc").value
+        // let fem = document.getElementById("fem").checked
+        // let masc = document.getElementById("masc").checked
 
-        if(nome == ""){
-            alert("O campo nome é obrigatório!")
+
+        if (nome == "") {
+           mensagem += "O campo nome é obrigatório!\n"
+        }
+
+        if(email == ""){
+            mensagem += "O campo e-mail é obrigatório!\n"
+        }
+
+        // if(fem == false && masc == false){
+        //     mensagem += "Selecione o sexo!\n"
+        // }
+
+        if(sexo == null){
+            mensagem += "Selecione o sexo!\n"
+        }
+
+        // if(qtdCursos == null){
+        //     mensagem += "Selecione um curso!\n"
+        // }
+
+        if(qtdCursos == 0){
+            mensagem += "Selecione um curso!\n"
+        }
+
+        if(estado == ""){
+            mensagem += "Selecione o estado!\n"
+        }
+
+        if(foto == undefined){
+            mensagem += "Selecione uma foto!\n"
+        } else if(foto.size >= 1000){
+            mensagem += "A foto deve ter no máximo 1 MB!\n"
+        }
+
+        if(dataNascimento == ""){
+            mensagem += "Data de nascimento obrigatória!"
+        }
+
+        //IMPRESSÃO DA MENSAGEM NO ALERTA
+        if(mensagem != ""){
+            alert(mensagem)
         }
     }
 }
