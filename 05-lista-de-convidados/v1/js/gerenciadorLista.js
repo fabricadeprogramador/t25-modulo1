485222e073793c971e4a67849a9aa861862dd7a8
Vinicius Souza <viniciuspodi@gmail.com>
2019-03-21 22:01:01 -0400

    Corrigindo Lista de convidados v1 e iniciando Dojo lista v2

--- 05-lista-de-convidados/v1/js/gerenciadorLista.js
+++ 05-lista-de-convidados/v1/js/gerenciadorLista.js
@@ -1,27 +1,39 @@
 class GerenciadorLista {
 
     adicionarConvidado() {
-     
+
         let ehValido = this.validar()
 
-        if(ehValido){
+        if (ehValido) {
             //Adiciono
             let convidado = document.getElementById('inputConvidado').value
             document.getElementById('lista').innerText += convidado + '\n'
-        } else {
-            alert("Preencha o nome do convidado!")
-        }
+            this.limpar()
+        } 
 
     }
 
     validar() {
-        if (document.getElementById("inputConvidado").value == "") {
+        let nomeConvidado = document.getElementById("inputConvidado").value
+        if (nomeConvidado == "") {
             //Inválido
+            alert("Preencha o nome do convidado!")
             return false
+        } else if (nomeConvidado.length > 30) {
+            alert("Nome deve conter no máximo 30 caracteres!")
+            return false
+        } else if (nomeConvidado.charAt(0) != nomeConvidado.charAt(0).toUpperCase()) {
+            alert("A primeira letra do nome deve ser maiúscula!")
+            return false
+        } else {
+            //Válido
+            return true
         }
 
-        //Válido
-        return true
+    }
+
+    limpar() {
+        document.getElementById('inputConvidado').value = ""
     }
 }
 
