0332b28e7161c3d1272d38ad8d309d54b405f989
Vinicius Souza <viniciuspodi@gmail.com>
2019-04-02 20:51:38 -0400

    Finalizando DOJO lista de convidados v2

--- 05-lista-de-convidados/v2/js/Gerenciador.js
+++ 05-lista-de-convidados/v2/js/Gerenciador.js
@@ -1,35 +1,41 @@
+var contador = 0
 class Gerenciador {
     adicionar() {
-        
+
         let Ehvalido = this.validar()
 
-        if (Ehvalido){
+        if (Ehvalido) {
 
-        let nomeConvidado = document.getElementById('inputConvidado').value
-        let div = document.createElement("div")
-        div.innerText = nomeConvidado
-        
-        let img = document.createElement("img")
-        img.setAttribute('onclick','gerenciador.remover()')
-        img.setAttribute('src',"img/lixeira.svg")
-        img.classList.add("imagem")
 
-        document.getElementById("lista").appendChild(div)
+            let nomeConvidado = document.getElementById('inputConvidado').value
+            let div = document.createElement("div")
+            div.innerText = nomeConvidado
+            div.classList.add("div")
+            div.setAttribute("id", "div-" + contador)
+
+            let img = document.createElement("img")
+            img.setAttribute('onclick', "gerenciador.remover('div-" + contador + "')")
+            img.setAttribute('src', "img/lixeira.svg")
+            img.classList.add("imagem")
+            img.classList.add("imagem:hover")
             div.appendChild(img)
-        this.limpar()
-        }
 
-        
+            let editar = document.createElement("img")
+
+            document.getElementById("lista").appendChild(div)
+            contador++
+            this.limpar()
+        }
     }
-    remover(){
-            
-    }        
+    remover(id) {
+        document.getElementById(id).remove()
+    }
     validar() {
         let nomeConvidado = document.getElementById('inputConvidado').value
         if (nomeConvidado == "") {
             alert("Digite o nome do convidado!")
             return false
-        } 
+        }
         return true
     }
     limpar() {
